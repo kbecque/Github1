0fc29d8ca0021a34d0fe0e407a2061ba94bc290e
Kurt Becqué <kurt.becque@donboscohalle.be>
2024-02-21 16:06:48 +0100

    test->test1

--- Github1/Program.cs
+++ Github1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test1");
+            Console.WriteLine("Eerste github classroom taak!");
         }
     }
 }
